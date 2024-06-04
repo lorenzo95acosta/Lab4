43461ea74cdfc3f32b37d69cc453a38ecb33a10c
lorenzo95acosta <88293738+lorenzo95acosta@users.noreply.github.com>
2024-06-04 12:07:46 -0300

    Agrega interfaces.h

---
+++ include/interfaces.h
@@ -0,0 +1,115 @@
+#ifndef _INTERFACES_H
+#define _INTERFACES_H
+
+#include <iostream>
+
+using namespace std;
+
+class IAltaListadoUsuario {
+  public:
+    virtual void nuevoUsuario(DTUsuario) = 0;
+    virtual DTInfoUsuarios listadoUsuarios() = 0;
+    virtual ~IAltaListadoUsuario(){};
+};
+
+class IAltaProducto {
+  public: 
+    virtual string listadoVendedores() = 0;
+    virtual void seleccionarVendedor(string) = 0;
+    virtual void ingresarProductos(string, float, int, string, DTCategoria) = 0;
+    virtual void confirmar() = 0;
+    virtual ~IAltaProducto(){};
+};
+
+class IConsultarProducto {
+  public:
+    virtual IDProducto listadoProductos() = 0;
+    virtual DTProducto mostrarProducto(string) = 0;
+    virtual ~IConsultarProducto(){};
+};
+
+class ICrearPromocion {
+  public:
+    virtual void ingresarDatosPromo(string, string, DTFecha) = 0;
+    virtual string listadoVendedores() = 0;
+    virtual void seleccionarVendedor(string) = 0;
+    virtual DTProducto listadoProductos() = 0;
+    virtual void ingresarProducto(int, int) = 0;
+    virtual void confirmar() = 0;
+    virtual ~ICrearPromocion(){};
+};
+
+class IConsultarPromocion {
+  public:
+    virtual DTPromo listadoPromociones() = 0;
+    virtual DTInfoProd seleccionarPromo(string) = 0;
+    virtual ~IConsultarPromocion(){};
+};
+
+class IRealizarCompra {
+  public:
+    virtual string listadoClientes() = 0;
+    virtual void seleccionarCliente(string) = 0;
+    virtual DTInfoProd mostrarProductos() = 0;
+    virtual void agregarProducto(string, int) = 0;
+    virtual DTCompra mostrarCompra() = 0;
+    virtual void confirmarCompra() = 0;
+    virtual ~IRealizarCompra(){};
+};
+
+class IDejarComentario {
+  public:
+    virtual string listadoUsuarios() = 0;
+    virtual void seleccionarUsuario(string) = 0;
+    virtual string listadoProductos() = 0;
+    virtual void seleccionarProducto(string) = 0;
+    virtual void nuevoComentario(string) = 0;
+    virtual void seleccionarComentario(DTComentario) = 0;
+    virtual void respuestaComentario(string) = 0;
+    virtual ~IDejarComentario(){};
+};
+
+class IEliminarComentario {
+  public:
+    virtual string listadoUsuarios() = 0;
+    virtual void seleccionarUsuario(string) = 0;
+    virtual DTComentario listarComentarios() = 0;
+    virtual void seleccionarComentario(DTComentario) = 0;
+    virtual void eliminarComentario() = 0;
+    virtual ~IEliminarComentario(){};
+};
+
+class IEnviarProducto {
+  public: 
+    virtual string listadoVendedores() = 0;
+    virtual void seleccionarVendedor(string) = 0;
+    virtual string listadoProductos() = 0;
+    virtual void seleccionarProducto(string) = 0;
+    virtual ListaPares listadoCompras() = 0;
+    virtual void seleccionarCompra(ListaPares) = 0;
+    virtual void marcarEnviado() = 0;
+    virtual ~IEnviarProducto(){};
+};
+
+class IExpedienteUsuario {
+  public:
+    virtual string listadoUsuarios() = 0;
+    virtual void seleccionarUsuario(string) = 0;
+    virtual DTInfoUsuario listarInfoUsuario() = 0;
+    virtual ~IExpedienteUsuario(){};
+};
+
+class INotificaciones {
+  public:
+    virtual string listarSinSuscripcion(string) = 0;
+    virtual void seleccionarVendedor(string) = 0;
+    virtual void agregarSuscripciones(string) = 0;
+    virtual DTNotificacion listarNotificacionesRecibidas(string) = 0;
+    virtual void eliminarNotificaciones(string) = 0;
+    virtual string listarSuscripcionesVend(string) = 0;
+    virtual void seleccionarVendedor(string) = 0;
+    virtual void eliminarSuscripciones(string) = 0;
+    virtual ~INotificaciones(){};
+};
+
+#endif
