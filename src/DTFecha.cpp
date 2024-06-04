b08ef5a33b82f6e6e2ce411e0b442d371f888cfc
lorenzo95acosta <88293738+lorenzo95acosta@users.noreply.github.com>
2024-06-04 11:26:43 -0300

    Agrega DTFecha

---
+++ src/DTFecha.cpp
@@ -0,0 +1,36 @@
+#include "../include/DTFecha.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+DTFecha::DTFecha(){
+	dia=-1;
+	mes=-1;
+	anio=-1;
+}
+
+DTFecha::DTFecha(int d,int m, int a){
+	dia=d;
+	mes=m;
+	anio=a;
+}
+
+string DTFecha::toString(){
+	string result=to_string(dia)+"/"+to_string(mes)+"/"+to_string(anio);
+	return result;
+}
+
+int DTFecha::getDia(){
+	return dia;
+}
+
+int DTFecha::getMes(){
+	return mes;
+}
+
+int DTFecha::getAnio(){
+	return anio;
+}
+
