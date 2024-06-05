8c98e4c0dac26346889918456303c30cfcaf68c7
FrancoTestore <francotestore2611@gmail.com>
2024-06-05 15:54:35 -0300

    Add files via upload

---
+++ include/ProducroEnCompra.h
@@ -0,0 +1,14 @@
+#ifndef _PRODUCTO_EN_COMPRA_H_
+#define _PRODUCTO_EN_COMPRA_H_
+
+using namespace std;
+
+#include "Compra.h"
+
+class productoEnCompra {
+    private:
+        int cantidadProducto;
+        float precioTotal;
+    public:
+        Compra Link-PC-C(Compra c);
+};
