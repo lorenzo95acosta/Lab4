8522ba761c5a5dfa5c4118bdaee0fd2a49f8b134
lorenzo95acosta <88293738+lorenzo95acosta@users.noreply.github.com>
2024-06-08 13:00:13 -0300

    Cambios directorios

---
+++ include/Alejandro/Compra.h
@@ -0,0 +1,26 @@
+#ifndef _COMPRA_H_
+#define _COMPRA_H_
+
+using namespace std;
+
+#include "DTFecha.h"
+
+class Compra {
+  private:
+    DTFecha fechaDeCompra;
+    bool productoEnviado;
+    float montoFinal;
+	public:
+		//constructor
+		Compra(DTFecha fechaDeCompra,bool productoEnviado, float montoFinal);
+
+		//getters
+		DTFecha getFechaCompra();
+		bool getProductoEnviado();
+		float getMontoFinal();
+
+		//destructor
+		~Compra();
+};
+
+#endif
